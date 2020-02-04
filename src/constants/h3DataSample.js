b6b2de245420629cd3130b87e1b50f2f6d5edb3d
Johannes Jarbratt <johannes.jarbratt+github@gmail.com>
2020-02-04 20:32:42 +0900

    DEV Sample File

---
+++ src/constants/h3DataSample.js
@@ -0,0 +1,151 @@
+export const h3Sample = {
+  datasets: {
+    info: {
+      id: 'f1tpeuf',
+      label: 'work'
+    },
+    data: {
+      fields: [
+        {
+          name: 'h3Id',
+          format: '',
+          tableFieldIndex: 1,
+          type: 'string'
+        },
+        {
+          name: 'count',
+          format: '',
+          tableFieldIndex: 2,
+          type: 'integer'
+        }
+      ],
+      rows: [
+        ['8b2f5aadc381fff', 1],
+        ['8b2f5aadc0d9fff', 0],
+        ['8b2f5aade222fff', 0],
+        ['8b2f5aadc384fff', 0],
+        ['8b2f5aadc3b3fff', 2],
+        ['8b2f5aadc3b0fff', 3],
+        ['8b2f5aadc226fff', 0],
+        ['8b2f5aada14afff', 0],
+        ['8b2e70caace0fff', 0],
+        ['8b2f5aadc39efff', 0],
+        ['8b2f5aac3acbfff', 0],
+        ['8b2f5aadb6f1fff', 0],
+        ['8b2f5aadc3aafff', 2],
+        ['8b2f5aae0a08fff', 0],
+        ['8b2f5a32d156fff', 0],
+        ['8b2f5aac1201fff', 0],
+        ['8b2f5aadc053fff', 0],
+        ['8b2f5aadc41bfff', 0],
+        ['8b2e74da0226fff', 0],
+        ['8b2e74bac435fff', 0],
+        ['8b2f58612b69fff', 0],
+        ['8b2f5aadc293fff', 1],
+        ['8b2f5a258685fff', 0],
+        ['8b2f5a329c25fff', 0],
+        ['8b2f5aadc393fff', 0],
+        ['8b2f5aac36d8fff', 0],
+        ['8b2f5aadc542fff', 0],
+        ['8b2f5aadc710fff', 0],
+        ['8b2f5bc819a8fff', 0],
+        ['8b2f5a32dd46fff', 0],
+        ['8b2f5aade1b1fff', 0],
+        ['8b2f5aadc3a2fff', 1],
+        ['8b2f5aadc192fff', 0],
+        ['8b2f5aadc386fff', 0],
+        ['8b2f5aadb2defff', 0],
+        ['8b2f5aadc39cfff', 0],
+        ['8b2f5aad30b3fff', 0],
+        ['8b2f5aadc0ecfff', 0],
+        ['8b2f5aadc385fff', 0],
+        ['8b2f5aadc3acfff', 0],
+        ['8b2f5aadc3a3fff', 0],
+        ['8b2f5a35c0e3fff', 0],
+        ['8b2f5aadcad0fff', 0],
+        ['8b2f5aac3a8bfff', 0],
+        ['8b2f5aadc395fff', 0],
+        ['8b2f5aadc388fff', 0],
+        ['8b2f5aadc313fff', 0],
+        ['8b2f5aad9041fff', 0],
+        ['8b2f5aadc0f3fff', 0],
+        ['8b2f5aadc740fff', 0],
+        ['8b2f5bd5808afff', 0],
+        ['8b2f5aadc38cfff', 0],
+        ['8b2f5bd9c946fff', 0],
+        ['8b2f5aadc01dfff', 0],
+        ['8b2f5aadc394fff', 0],
+        ['8b2f5a32cbabfff', 0],
+        ['8b2f5a148662fff', 0],
+        ['8b2f5a221a73fff', 0],
+        ['8b2f5aadcaeefff', 0],
+        ['8b2f5aadca90fff', 0],
+        ['8b2f5aad2b13fff', 0],
+        ['8b2f5aadc05bfff', 0],
+        ['8b2f5aad139afff', 0],
+        ['8b2f5aadb712fff', 0],
+        ['8b2f5a312db0fff', 0],
+        ['8b2f5a32d066fff', 0],
+        ['8b2f5aadc3b2fff', 1],
+        ['8b2f5a32caddfff', 0],
+        ['8b2f5aadcad5fff', 0],
+        ['8b2f5a37594dfff', 0],
+        ['8b2f5aadc0edfff', 0],
+        ['8b2e44985ce6fff', 0],
+        ['8b2f5a32d16dfff', 0],
+        ['8b2f5a308ad0fff', 0],
+        ['8b2f5aac3391fff', 0],
+        ['8b2f5aadc38dfff', 0],
+        ['8b2f5aacc855fff', 0],
+        ['8b2f5aade49cfff', 0],
+        ['8b2f5aad321dfff', 0],
+        ['8b2f5aadc188fff', 0],
+        ['8b2f5aae8351fff', 0],
+        ['8b2f5a32c288fff', 0],
+        ['8b2f5aa8a859fff', 0],
+        ['8b2f5a16b16dfff', 0],
+        ['8b2f5aadc328fff', 0],
+        ['8b2f5aadcb75fff', 0],
+        ['8b2f5aaca4a1fff', 0],
+        ['8b2f5aad8575fff', 0],
+        ['8b2f5aa49673fff', 0],
+        ['8b2f5a375215fff', 0],
+        ['8b2f5aa5040efff', 0]
+      ]
+    }
+  },
+  options: {
+    centerMap: false,
+    keepExistingConfig: true
+  },
+  config: {
+    visState: {
+      filters: [],
+      layers: [
+        {
+          type: 'hexagonId',
+          config: {
+            dataId: 'f1tpeuf',
+            label: 'work',
+            columns: {
+              hex_id: 'h3Id'
+            },
+            colorScale: 'ordinal',
+            colorField: {
+              name: 'count',
+              type: 'integer'
+            },
+            isVisible: true,
+            visConfig: {
+              coverage: 1,
+              opacity: 0.51,
+              colorRange: {
+                colors: ['#DDB27C', '#12939A', '#88572C', '#FF991F']
+              }
+            }
+          }
+        }
+      ]
+    }
+  }
+};
